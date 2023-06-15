38cfff5b2380f75712a54720b5dd489fc0968960
Dilcia19 <dilc93@gmail.com>
2023-06-15 17:14:15 +0000

    removed arrests file to walk through script

--- clean/src/dilcia.py
+++ clean/src/dilcia.py
@@ -11,3 +11,17 @@ logging.basicConfig(
 )
 
 # going through script line by line to make sure I understand the code & filters
+
+
+def main(*input_files, output_file):
+    # some code
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--year-files", "-f", nargs="+", action="append", required=True)
+    parser.add_argument("--output-file", "-o", required=True)
+    args = parser.parse_args()
+
+    main(*args.year_files, output_file=args.output_file)
